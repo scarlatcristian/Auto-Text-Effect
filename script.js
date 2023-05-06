7b46ebadd7d73c6c0cf71015c96c20d50c06f752
cristi <scarlat.cristian.gabriel@gmail.com>
2023-05-06 19:52:15 +0300

    added CSS and JS

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+const text = "We Love Programming!";
+let index = 1;
+let speed = 300 / speedEl.value;
+
+const writeText = () => {
+  textEl.innerText = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) {
+    index = 1;
+  }
+
+  setTimeout(writeText, speed);
+};
+
+speedEl.addEventListener("input", (e) => {
+  speed = 300 / e.target.value;
+});
+
+writeText();
